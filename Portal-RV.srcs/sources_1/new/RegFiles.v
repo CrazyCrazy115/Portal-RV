9f7c078c81c4a85ed6f5050c55e3c7e2d8b72163
旧念 <1114608610@qq.com>
2023-12-23 16:13:04 +0800

    first commit

---
+++ Portal-RV.srcs/sources_1/new/RegFiles.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/12/23 16:08:53
+// Design Name: 
+// Module Name: RegFiles
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RegFiles(
+
+    );
+endmodule
